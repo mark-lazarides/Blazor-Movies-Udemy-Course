fbf796e9e7bdb1f68d9909b371395a62e3cc5a96
Mark Lazarides <mark@intecre.com>
2020-01-18 09:07:50 +0000

    WIP commit

---
+++ Shared/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+  public class Movie
+  {
+    [Required(ErrorMessage = "Movie title is required.")]
+    [StringLength(maximumLength: 15, ErrorMessage = "Max length is 15 characters.")]
+    public string Title { get; set; }
+    [Required(ErrorMessage = "Release date is required.")]
+    public DateTime ReleaseDate { get; set; }
+  }
+}
